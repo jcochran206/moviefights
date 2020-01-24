3ac653b51a7c25b432118902002676ec80a7e5bb
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2020-01-25 00:06:04 +0430

    modified js to fetch data and iterate movies and place them on index

--- js/app.js
+++ js/app.js
@@ -1,21 +1,31 @@
-//fetch data from omdbapi using axios fetch
 const fetchDAta = async (searchTerm) => {
-  axios.get('http://www.omdbapi.com/', {
+   const response = await axios.get('http://www.omdbapi.com/', {
       params: {
         s: searchTerm,
-        apikey: 'your_api_here',
+        apikey: 'your api key'
       }
-    })
-    .then(function (response) {
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
     });
+    // to handle error for missing movies
+    if (response.data.Error){
+      return [];
+    }
+    return response.data.Search;
 };
+// create input variable
+const input = document.querySelector('input');
+//iterate thry movies objects from api
+const onInput = async event => {
+  const movies = await fetchDAta(event.target.value);
 
-const onInput = event => {
-  fetchDAta(event.target.value);
-};
+  for(let movie of movies) {
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <img src="${movie.Poster}" />
+    <h1>${movie.Title}<h1>
+    `;
+
+    document.querySelector('#target').appendChild(div);
+    }
+  };
 //add listener to wait for user interaction
 input.addEventListener('input', debounce(onInput, 1000));
