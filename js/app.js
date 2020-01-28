2df8363bc1044befcbfab75308578ed2f834786a
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2020-01-28 23:37:48 +0430

    third refactormoved fetchdata into autocomplete and made it more reusable

--- js/app.js
+++ js/app.js
@@ -1,82 +1,40 @@
-const fetchDAta = async (searchTerm) => {
-   const response = await axios.get('http://www.omdbapi.com/', {
-      params: {
-        s: searchTerm,
-        apikey: 'your-api-key'
-      }
-    });
-    // to handle error for missing movies
-    if (response.data.Error){
-      return [];
-    }
-    return response.data.Search;
-};
-
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-  <label><b> Search for movie </b></label>
-  <input type="text" class="input" placeholder="search for movie">
-  <div class="dropdown">
-    <div class="dropdown-menu">
-      <div class="dropdown-content results"></div>
-    </div>
-  </div>
-`;
-
-// create variable for selectors
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-
-//iterate thry movies objects from api
-const onInput = async event => {
-  const movies = await fetchDAta(event.target.value);
-
-  if(!movies.length) {
-    dropdown.classList.remove('is-active');
-    return;
-  }
-  //results to container to clear out data
-  resultsWrapper.innerHTML = '';
-  dropdown.classList.add('is-active');
-  for(let movie of movies) {
-    const option = document.createElement('a');
+createAutoComplete({
+  root: document.querySelector('.autocomplete'),
+  renderOption(movie) {
     const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster; //tenary for
-
-    option.classList.add('dropdown-item');
-    option.innerHTML = `
+    return `
     <img src="${imgSrc}" />
-    ${movie.Title}
+    ${movie.Title} (${movie.Year})
     `;
-    // updates text on search when clicked on search
-    option.addEventListener('click', ()=> {
-      dropdown.classList.remove('is-active');
-      input.value = movie.Title;
-      // helper function to create movie details in index
-      onMovieSelect(movie);
-
-    });
-
-
-
-    resultsWrapper.appendChild(option);
-    }
-  };
-//add listener to wait for user interaction
-input.addEventListener('input', debounce(onInput, 1000));
-// closes search bar when on user clicks
-document.addEventListener('click', event => {
-  if(!root.contains(event.target)){
-    dropdown.classList.remove('is-active');
+  },
+  onOptionSelect(movie){
+    onMovieSelect(movie)
+  },
+  inputValue(movie){
+    return movie.Title;
+  },
+  async fetchData(searchTerm) {
+     const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+          s: searchTerm,
+          apikey: 'your api key'
+        }
+      });
+      // to handle error for missing movies
+      if (response.data.Error){
+        return [];
+      }
+      return response.data.Search;
   }
 });
+
+
 // helper function to grab id of movie to obtain details
 const onMovieSelect = async movie => {
   const response = await axios.get('http://www.omdbapi.com/', {
      params: {
        i: movie.imdbID,
-       apikey: 'your-api-key'
+       apikey: 'your api key'
      }
    });
 
--- js/autocomplete.js
+++ js/autocomplete.js
@@ -1,3 +1,58 @@
-const createAutocomplete = () => {
-  console.log('here is option3')
+const createAutoComplete = ({ root, renderOption, onOptionSelect, inputValue, fetchData }) => {
+
+  root.innerHTML = `
+    <label><b> Search </b></label>
+    <input type="text" class="input" placeholder="search for movie">
+    <div class="dropdown">
+      <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+      </div>
+    </div>
+  `;
+
+  // create variable for selectors
+  const input = root.querySelector('input');
+  const dropdown = root.querySelector('.dropdown');
+  const resultsWrapper = root.querySelector('.results');
+
+
+  //iterate thry movies objects from api
+  const onInput = async event => {
+    const items = await fetchData(event.target.value);
+
+    if(!items.length) {
+      dropdown.classList.remove('is-active');
+      return;
+    }
+    //results to container to clear out data
+    resultsWrapper.innerHTML = '';
+    dropdown.classList.add('is-active');
+    for(let item of items) {
+      const option = document.createElement('a');
+      const imgSrc = item.Poster === 'N/A' ? '' : item.Poster; //tenary for
+
+      option.classList.add('dropdown-item');
+      option.innerHTML = renderOption(item);
+      // updates text on search when clicked on search
+      option.addEventListener('click', ()=> {
+        dropdown.classList.remove('is-active');
+        input.value = inputValue(item);
+        // helper function to create item details in index
+        onOptionSelect(item);
+
+      });
+
+
+
+      resultsWrapper.appendChild(option);
+      }
+    };
+  //add listener to wait for user interaction
+  input.addEventListener('input', debounce(onInput, 1000));
+  // closes search bar when on user clicks
+  document.addEventListener('click', event => {
+    if(!root.contains(event.target)){
+      dropdown.classList.remove('is-active');
+    }
+  });
 };
